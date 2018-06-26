41b76e3d01155e2e2280528db931d7c41ba18d4b
Damien Erambert <damien.erambert@me.com>
2018-06-25 21:49:28 -0700

    :shirt:

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   rules: {
     'import/no-extraneous-dependencies': 0,
+    'import/prefer-default-export': 0,
     'react/jsx-filename-extension': 0,
     'jsx-a11y/anchor-is-valid': 0,
     'jsx-a11y/accessible-emoji': 0,
